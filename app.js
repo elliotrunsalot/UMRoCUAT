e1c59023d8210c61b89727f57350d9354e8d3e65
Elliot Cooper <37920832+elliotrunsalot@users.noreply.github.com>
2022-09-24 22:12:49 +1000

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const navSlide = () => {
-	const burger = document.querySelector('.burger');
-	const nav = document.querySelector('.nav22');
-	const navLinks = document.querySelectorAll('.nav22 li');
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav23");
+  const navLinks = document.querySelectorAll(".nav23 li");
 
-	burger.addEventListener('click',() => {
-		//Toggle Nav
-		nav.classList.toggle('nav-active');
-		
-		//Animate Links
-		navLinks.forEach((link, index) => {
-			if (link.style.animation){
-				link.style.animation = '';
-			} else {
-				link.style.animation = `navlinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
-			}
-		});
-		//Burger Animation
-		burger.classList.toggle('toggle');
-	});
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navlinkFade 1s ease forwards ${
+          index / 7 + 1.5
+        }s`;
+      }
+    });
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
 };
 
 navSlide();
